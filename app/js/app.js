9ca57fca2ab41cb3f0a10e51f377ff876eeb74e4
michal <michterlecki@gmail.com>
2020-04-02 12:24:11 +0200

    v1.16 form

--- app/js/app.js
+++ app/js/app.js
@@ -24,6 +24,7 @@ $(document).ready(function(e) {
         },
     });
 
+    $('[data-toggle="popover"]').popover();
 
     const html = document.documentElement;
     const navbarToggler = $('#navbarSupportedContent');
